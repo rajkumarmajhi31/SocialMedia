f3853ce2ffd52065801eefdcf56218f467b8d7e9
rajkumarmajhi31 <rajkumarmajhi1122@gmail.com>
2024-09-29 20:04:56 +0530

    Updated UI

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          BuddyUp
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
